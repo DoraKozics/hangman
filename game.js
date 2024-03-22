63e9074ad9737763b45dd38e31f6d65658664335
kozic <kozicsdora@gmail.com>
2024-03-22 11:48:10 +0100

    make letters guessable & displayed

--- game.js
+++ game.js
@@ -1,14 +1,16 @@
+let wordLength = 6;
 let wordDisplay;
 let keyboard;
 let wordToGuess;
+let amountOfLettersToGuess = wordLength;
 document.addEventListener('DOMContentLoaded', () => {
     wordDisplay = document.getElementById('word-display');
     keyboard = document.getElementById('keyboard');
     generateKeyboard();
     generateNewWord()
         .then(word => {
-            console.log("Random word:", word);
-            wordToGuess = word;
+            wordToGuess = word.toUpperCase();
+            console.log(wordToGuess);
             displayNewWord();
         })
         .catch(error => {
@@ -26,12 +28,17 @@ const generateKeyboard = () => {
         let character = alphabet[i];
         let button = document.createElement("button");
         button.innerText = character;
+
+        button.onclick = function (e) {
+            checkForLetterAppearance(character);
+            button.onclick = null;
+        }
         keyboard.appendChild(button);
     }
 }
 
 const generateNewWord = () => {
-    return fetch("https://random-word-api.herokuapp.com/word?length=6")
+    return fetch("https://random-word-api.herokuapp.com/word?length=" + wordLength)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -50,7 +57,38 @@ const displayNewWord = () => {
     for (let i = 0; i < wordToGuess.length; i++) {
         const letter = document.createElement('div');
         letter.classList.add('word-letter');
-        // letter.innerText = wordToGuess[i];
+        letter.setAttribute('id', 'word-letter-' + i);
         wordDisplay.appendChild(letter);
+    }
+}
+
+const checkForLetterAppearance = (character) => {
+    for (let i = 0; i < wordToGuess.length; i++) {
+        if (wordToGuess[i] === character) {
+            console.log("contains");
+            document.getElementById('word-letter-' + i).innerText = character;
+            amountOfLettersToGuess--;
+        }
+    }
+    checkForSolvedWord();
+}
+
+const checkForSolvedWord = () => {
+    if (amountOfLettersToGuess === 0) {
+        console.log("solved!");
+        fetch("https://api.dictionaryapi.dev/api/v2/entries/en/" + wordToGuess)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Process the response data
+                console.log(data);
+                // Depending on the structure of the response, you might access the definition like:
+                // const definition = data[0].meanings[0].definitions[0].definition;
+                // console.log(definition);
+            })
     }
 }
